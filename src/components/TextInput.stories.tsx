13a02ea6cfaed4e8a4949bf32c9226299ae61f05
henriqueneubert <neubert.study@gmail.com>
2022-10-12 20:11:31 -0300

    feat: progress in component TextInput

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -4,7 +4,10 @@ import { TextInput, TextInputProps } from './TextInput'
 export default { /*cria uma 'categoria' no storybook*/
   title: 'Components/TextInput',/*categoria/subcategoria*/
   component: TextInput, /*recebe text, que vem de Text.tsx*/
-  args:{},
+  args:{
+    placeholder: 'Type your e-mail address',
+    type: 'email'
+  },
   argTypes: {}
 
 } as Meta<TextInputProps>
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,16 +1,18 @@
-import { ReactNode } from 'react';
+import { InputHTMLAttributes } from 'react';
 import { clsx } from 'clsx'; /*add a opção de verificação e estipular default*/
 
-export interface TextInputProps { /*tipos de tamanho para os ButtonOS*/
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement>{ /*tipos de tamanho para os ButtonOS*/
+  placeholder?: string
 }
 
 /*default = md*/
-export function TextInput({}: TextInputProps) /*recebe o size*/
+export function TextInput(props: TextInputProps) /*recebe o size*/
 {
   return (
     <input className={clsx(
       'py-4 px-3 rounded bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400 w-full', /*default*/
-    )}    
+    )}  
+    {...props}  
     />
   )
 }
