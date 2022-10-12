a219be4bf359aeaea3889cd33658e1ffff4d5dd8
henriqueneubert <neubert.study@gmail.com>
2022-10-12 17:13:29 -0300

    feat: added defaut - fontSize, colors and fontFamily. in tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,33 @@ module.exports = {
   ],
   theme: {
     extend: {
+
+      fontSize:{
+        xs:14,
+        sm:16,
+        md:18,
+        lg:20,
+        xl:24,
+        '2xl':32,
+      },
+
+      colors:{
+        'black':'#000000',
+        'white':'#ffffff',
+
+        'gray-900':'#121214',
+        'gray-800':'#202024',
+        'gray-400':'#7C7C8A',
+        'gray-200':'#C4C4CC',
+
+        'cyan-500':'#81D8F7',
+        'cyan-300':'#9BE1FB',
+      },
+
       fontFamily: {
         sans: 'Inter, sans-serif'
       },
+
     },
   },
   plugins: [],
